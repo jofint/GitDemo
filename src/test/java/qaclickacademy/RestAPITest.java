0f3b28783f60983b15aa1fee44864aa5ee80376e
Anita <jofin00@gmail.com>
2020-12-03 14:05:38 +0530

    First project

---
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -0,0 +1,25 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+
+		// TODO Auto-generated method stub
+		
+		@Test
+		public void PostJira()
+		{
+			System.out.println("PostJira");
+
+		}
+		@Test
+		public void Deletetwitter()
+		{
+			System.out.println("Deletetwitter");
+
+		}
+		
+
+	
+
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,27 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+		
+		@Test
+		public void browser()
+		{
+			System.out.println("Elementsui");
+
+		}
+		
+
+		
+		@Test
+		public void Elementsui()
+		{
+			System.out.println("Elementsui");
+
+		}
+		
+
+	
+
+}
